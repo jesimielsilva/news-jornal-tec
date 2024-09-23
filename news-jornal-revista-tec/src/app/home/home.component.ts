a1cc21de976e08e4da282a6058b90e441933ac5c
Jesimiel <jesimielbarbosa@gmail.com>
2024-09-22 23:02:48 -0300

    commented function on the front

--- news-jornal-revista-tec/src/app/home/home.component.ts
+++ news-jornal-revista-tec/src/app/home/home.component.ts
@@ -13,19 +13,28 @@ export class HomeComponent {
   ngInit(){}
   
   downloadPdf() {
+    // Faz uma requisição GET para o endpoint da API que gera o PDF
+    // A opção responseType: 'blob' indica que esperamos uma resposta binária (arquivo)
     this.http.get('http://localhost:8080/api/generate-pdf', { responseType: 'blob' })
       .subscribe((response: Blob) => {
+        // Cria uma URL temporária para o Blob recebido
         const url = window.URL.createObjectURL(response);
+        // Cria um elemento <a> dinamicamente
         const a = document.createElement('a');
+        // Define o href da tag <a> para a URL temporária criada
         a.href = url;
+        // Define o nome do arquivo que será baixado como 'jornal-noticias.pdf'
         a.download = 'jornal-noticias.pdf';
+        // Adiciona o elemento <a> ao corpo do documento
         document.body.appendChild(a);
+        // Simula um clique no link <a> para iniciar o download
         a.click();
+        // Remove o elemento <a> do DOM após o clique para limpar o documento
         document.body.removeChild(a);
       });
   }
 
-  baixarPdf() {
+  /*baixarPdf() {
     const conteudo = document.getElementById('conteudo-pdf')?.innerHTML || '';
 
     const body = { html: conteudo };
@@ -40,6 +49,6 @@ export class HomeComponent {
         a.click();
         document.body.removeChild(a);
       });
-  }
+  }*/
 
 }
